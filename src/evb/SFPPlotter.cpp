5d734160902c905deb30afe2af4e194359485526
alconley <imaconley1@gmail.com>
2022-08-17 18:07:51 -0400

    modified:   src/evb/SFPPlotter.cpp

--- src/evb/SFPPlotter.cpp
+++ src/evb/SFPPlotter.cpp
@@ -237,15 +237,15 @@ namespace EventBuilder {
 		for(int i=0; i<5; i++){
 			if(ev.cebraE[i] != -1) {
 				if(i==0){
-					MyFill(table,"cebraE.0_noCuts_noShift",4096,0,4096,ev.cebraE[0]);
+					MyFill(table,"cebraE.0_noCuts",4096,0,4096,ev.cebraE[0]);
 				} else if(i==1) {
-					MyFill(table,"cebraE.1_noCuts_noShift",4096,0,4096,ev.cebraE[1]);
+					MyFill(table,"cebraE.1_noCuts",4096,0,4096,ev.cebraE[1]);
 				} else if(i==2) {
-					MyFill(table,"cebraE.2_noCuts_noShift",4096,0,4096,ev.cebraE[2]);
+					MyFill(table,"cebraE.2_noCuts",4096,0,4096,ev.cebraE[2]);
 				} else if(i==3) {
-					MyFill(table,"cebraE.3_noCuts_noShift",4096,0,4096,ev.cebraE[3]);
+					MyFill(table,"cebraE.3_noCuts",4096,0,4096,ev.cebraE[3]);
 				} else if(i==4) {
-					MyFill(table,"cebraE.4_noCuts_noShift",4096,0,4096,ev.cebraE[4]);
+					MyFill(table,"cebraE.4_noCuts",4096,0,4096,ev.cebraE[4]);
 				} 
 
 			MyFill(table,"cebraE_NoCuts",4096,0,4096,ev.cebraE[i]);
@@ -354,7 +354,7 @@ namespace EventBuilder {
 		// 	}
 		// }
 
-		Double_t cebraRelT_1_toScint = ev.cebraTime[0] - ev.scintLeftTime;
+				Double_t cebraRelT_1_toScint = ev.cebraTime[0] - ev.scintLeftTime;
      	Double_t cebraRelT_2_toScint = ev.cebraTime[1] - ev.scintLeftTime;
 		Double_t cebraRelT_3_toScint = ev.cebraTime[2] - ev.scintLeftTime;
 		Double_t cebraRelT_4_toScint = ev.cebraTime[3] - ev.scintLeftTime;
@@ -365,51 +365,85 @@ namespace EventBuilder {
 		Double_t cebraRelT_1_to_4 = cebraRelT_1_toScint - cebraRelT_4_toScint;
 		Double_t cebraRelT_1_to_5 = cebraRelT_1_toScint - cebraRelT_5_toScint;
 
- 		if(ev.cebraE[0]!=-1 && ev.cebraE[1]!=-1){
-			MyFill(table,"cebraE0_vs_cebraE1_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[1]);
-			MyFill(table,"cebraTime0-cebraTime1_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[1]);
-			MyFill(table,"cebraE1_vs_cebraTime0-cebraTime1_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[1],4096,0,4096,ev.cebraE[1]);
-			}
+		double cebra_0_1_TimeShift = 3.0;
+		double cebra_0_2_TimeShift = 3.0;
+		double cebra_0_3_TimeShift = 68.0;
+		double cebra_0_4_TimeShift = 80.0;
 
-		if(ev.cebraE[0]!=-1 && ev.cebraE[2]!=-1){
-			MyFill(table,"cebraE0_vs_cebraE2_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[2]);
-			MyFill(table,"cebraTime0-cebraTime2_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[2]);
-			MyFill(table,"cebraE2_vs_cebraTime0-cebraTime2_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[2],4096,0,4096,ev.cebraE[2]);
-			}
+		int time_window = 20;
+		int full_time_window = time_window*2;
 
-		if(ev.cebraE[0]!=-1 && ev.cebraE[3]!=-1){
-			MyFill(table,"cebraE0_vs_cebraE3_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[3]);
-			MyFill(table,"cebraTime0-cebraTime3_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[3]);
-			MyFill(table,"cebraE3_vs_cebraTime0-cebraTime3_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[3],4096,0,4096,ev.cebraE[3]);
-			}
+		if(ev.cebraTime[0]-ev.cebraTime[1]+cebra_0_1_TimeShift >-time_window && ev.cebraTime[0]-ev.cebraTime[1]+cebra_0_1_TimeShift <time_window){
+			if(ev.cebraE[0]!=-1 && ev.cebraE[1]!=-1){
+				MyFill(table,"cebraE0_vs_cebraE1_wTimeGate_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[1]);
+				MyFill(table,"cebraTime0-cebraTime1_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[1]+cebra_0_1_TimeShift);
+				MyFill(table,"cebraE1_vs_cebraTime0-cebraTime1_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[1]+cebra_0_1_TimeShift,4096,0,4096,ev.cebraE[1]);
+				}
+		}
 
-		if(ev.cebraE[0]!=-1 && ev.cebraE[4]!=-1){
-			MyFill(table,"cebraE0_vs_cebraE4_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[4]);
-			MyFill(table,"cebraTime0-cebraTime4_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[4]);
-			MyFill(table,"cebraE4_vs_cebraTime0-cebraTime4_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[4],4096,0,4096,ev.cebraE[4]);
-			}
+		if(ev.cebraTime[0]-ev.cebraTime[2]+cebra_0_2_TimeShift >-time_window && ev.cebraTime[0]-ev.cebraTime[2]+cebra_0_2_TimeShift <time_window){
+			if(ev.cebraE[0]!=-1 && ev.cebraE[2]!=-1){
+				MyFill(table,"cebraE0_vs_cebraE2_wTimeGate_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[2]);
+				MyFill(table,"cebraTime0-cebraTime2_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[2]+cebra_0_2_TimeShift);
+				MyFill(table,"cebraE2_vs_cebraTime0-cebraTime2_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[2]+cebra_0_2_TimeShift,4096,0,4096,ev.cebraE[2]);
+				}
+		}
+
+		if(ev.cebraTime[0]-ev.cebraTime[3]+cebra_0_3_TimeShift >-time_window && ev.cebraTime[0]-ev.cebraTime[3]+cebra_0_3_TimeShift <time_window){
+			if(ev.cebraE[0]!=-1 && ev.cebraE[3]!=-1){
+				MyFill(table,"cebraE0_vs_cebraE3_wTimeGate_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[3]);
+				MyFill(table,"cebraTime0-cebraTime3_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[3]+cebra_0_3_TimeShift);
+				MyFill(table,"cebraE3_vs_cebraTime0-cebraTime3_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[3]+cebra_0_3_TimeShift,4096,0,4096,ev.cebraE[3]);
+				}
+		}
+
+		if(ev.cebraTime[0]-ev.cebraTime[4]+cebra_0_4_TimeShift >-time_window && ev.cebraTime[0]-ev.cebraTime[4]+cebra_0_4_TimeShift <time_window){
+			if(ev.cebraE[0]!=-1 && ev.cebraE[4]!=-1){
+				MyFill(table,"cebraE0_vs_cebraE4_wTimeGate_Cut",4096,0,4096,ev.cebraE[0],4096,0,4096,ev.cebraE[4]);
+				MyFill(table,"cebraTime0-cebraTime4_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[4]+cebra_0_4_TimeShift);
+				MyFill(table,"cebraE4_vs_cebraTime0-cebraTime4_wTimeGate_shifted_Cut",full_time_window,-time_window,time_window,ev.cebraTime[0]-ev.cebraTime[4]+cebra_0_4_TimeShift,4096,0,4096,ev.cebraE[4]);
+				}
+		}
 
 
+		// Fills plots with the full CeBrA Coinciedence window
+			if(ev.cebraE[0]!=-1 && ev.cebraE[1]!=-1){
+				MyFill(table,"cebraTime0-cebraTime1_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[1]);
+				}
+
+			if(ev.cebraE[0]!=-1 && ev.cebraE[2]!=-1){
+				MyFill(table,"cebraTime0-cebraTime2_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[2]);
+				}
+
+			if(ev.cebraE[0]!=-1 && ev.cebraE[3]!=-1){
+				MyFill(table,"cebraTime0-cebraTime3_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[3]);
+				}
+
+			if(ev.cebraE[0]!=-1 && ev.cebraE[4]!=-1){
+				MyFill(table,"cebraTime0-cebraTime4_Cut",3000,-1500,1500,ev.cebraTime[0]-ev.cebraTime[4]);
+				}
 
 
 		int count = 0;
 		for(int i=0; i<5; i++){
 			if(ev.cebraE[i] != -1) {
 				if(i==0){
-					MyFill(table,"cebraE.0_Cut_noShift",4096,0,4096,ev.cebraE[0]);
+					MyFill(table,"cebraE.0_Cut",4096,0,4096,ev.cebraE[0]);
 				} else if(i==1) {
-					MyFill(table,"cebraE.1_Cut_noShift",4096,0,4096,ev.cebraE[1]);
+					MyFill(table,"cebraE.1_Cut",4096,0,4096,ev.cebraE[1]);
 				} else if(i==2) {
-					MyFill(table,"cebraE.2_Cut_noShift",4096,0,4096,ev.cebraE[2]);
+					MyFill(table,"cebraE.2_Cut",4096,0,4096,ev.cebraE[2]);
 				} else if(i==3) {
-					MyFill(table,"cebraE.3_Cut_noShift",4096,0,4096,ev.cebraE[3]);
+					MyFill(table,"cebraE.3_Cut",4096,0,4096,ev.cebraE[3]);
 				} else if(i==4) {
-					MyFill(table,"cebraE.4_Cut_noShift",4096,0,4096,ev.cebraE[4]);
+					MyFill(table,"cebraE.4_Cut",4096,0,4096,ev.cebraE[4]);
 				} 
 
-			MyFill(table,"cebraE_Cut_noshifts",4096,0,4096,ev.cebraE[i]);
+			MyFill(table,"cebraE_Cut",4096,0,4096,ev.cebraE[i]);
 			//change the range to whatever the global id range is for the detectors on the channel map
-			MyFill(table,"cebraChannel_cebraE_Cut_noShifts",20,0,20,ev.cebraChannel[i],4096,0,4096,ev.cebraE[i]);
+			MyFill(table,"cebraChannel_cebraE_Cut",20,0,20,ev.cebraChannel[i],4096,0,4096,ev.cebraE[i]);
+
+
 
 	
 			} else {
